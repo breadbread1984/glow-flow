48704ca4f2d768d060b8d7d594dc55050d79fe04
解易_Xieyi <yi.xie@enflame.cn>
2018-12-22 12:57:00 +0800

    modified:   ActNorm.py

--- ActNorm.py
+++ ActNorm.py
@@ -5,9 +5,11 @@ import tensorflow as tf;
 import tensorflow_probability as tfp;
 
 class ActNorm(tfp.bijectors.Bijector):
+
     def __init__(self, validate_args = False, name = 'actnorm'):
         super(ActNorm,self).__init__(forward_min_event_ndims = 3, validate_args = validate_args, name = name);
         self.initialized = False;
+
     def getStatics(self, input):
         assert issubclass(type(input),tf.Tensor);
         #expectation
@@ -16,6 +18,7 @@ class ActNorm(tfp.bijectors.Bijector):
         variance = tf.math.reduce_mean(tf.math.square(input - mean), axis = [0,1,2]);
         stdvar = tf.math.sqrt(variance);
         return mean,stdvar;
+
     def _forward(self, x):
         #initialize with the first batch
         if self.initialized == False:
@@ -24,6 +27,7 @@ class ActNorm(tfp.bijectors.Bijector):
             self.scale = tf.Variable(1. / (stdvar + 1e-6), name = 'stdvar');
             self.initialized = True;
         return (x + self.loc) * self.scale;
+
     def _inverse(self, y):
         #initialize with the first batch
         if self.initialized == False:
@@ -32,6 +36,7 @@ class ActNorm(tfp.bijectors.Bijector):
             self.scale = tf.Variable(stdvar, name = 'stdvar');
             self.initialized = True;
         return y / self.scale - self.loc;
+
     def _inverse_log_det_jacobian(self, y):
         #df^{-1}(y) / dy = 1 / stdvar
         #ildj = log(abs(diag(1/stdvar))), where diag(1/stdvar) is a (h*w) x (h*w) matrix
